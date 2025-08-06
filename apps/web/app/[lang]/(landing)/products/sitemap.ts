96f51ead59fa785bdcc24aa3d11c203d56eb5345
alin <wsyanligang@gmail.com>
2025-08-05 22:59:53 -0300

    fix

--- apps/web/app/[lang]/(landing)/products/sitemap.ts
+++ apps/web/app/[lang]/(landing)/products/sitemap.ts
@@ -1,6 +1,6 @@
 import type {MetadataRoute} from 'next'
 import {i18n} from "@/config/i18n-config";
-import {api } from "@repo/shared/api-client"
+import {api} from "@repo/shared/api-client"
 
 export async function generateSitemaps() {
   return i18n.locales.map((lang) => {
@@ -14,7 +14,8 @@ export default async function sitemap(props: {
   id: string
 }): Promise<MetadataRoute.Sitemap> {
   const result: any[] = []
-  const allSlugs = await api.products.findAllSlug()
+  // const allSlugs = await api.products.findAllSlug()
+  const allSlugs: string[] = []
   for (const slug of allSlugs) {
     result.push({
       url: `${process.env.NEXT_PUBLIC_DOMAIN_URL}/${props.id}/products/${slug}`,
